e023ced4ec0f8ce0389b1b055031f5df2dc03fcd
Oxford <ox_harris@yahoo.com>
2023-03-25 10:21:56 +0100

    Fix: how document URL is obtained in resource loader

--- src/SelectiveResourceLoader.js
+++ src/SelectiveResourceLoader.js
@@ -19,13 +19,13 @@ export default class SelectiveResourceLoader extends Jsdom.ResourceLoader {
      */
     async fetch( url, options ) {
         if ( !options.element.hasAttribute( 'ssr' ) || !options.element.hasAttribute( 'src' ) ) return;
-        const baseUrl = options.element.ownerDocument.location;
+        const baseUrl = options.element.ownerDocument.URL;
         const src = options.element.getAttribute( 'src' );
-        if ( baseUrl.href.startsWith( 'file:/' ) && ( !src.includes( ':' ) || ![ 'file', 'http', 'https', ].includes( src.split( ':' )[ 0 ] ) ) ) {
+        if ( baseUrl.startsWith( 'file:/' ) && ( !src.includes( ':' ) || ![ 'file', 'http', 'https', ].includes( src.split( ':' )[ 0 ] ) ) ) {
             // Notice we are not relying on document-resolved url here.
             // E.g. if baseUrl is "file:///C:/path" and element src is "/file.ext", document-resolved url will be "file:///C:/file.ext" but we want "file:///C:/path/file.ext"
             // Notice we do not want to use the Path utility here. Destroys the file:/// url convention in windows
-            url = `${ baseUrl.href }/${ src }`;
+            url = `${ baseUrl }/${ src }`;
         }
         return super.fetch( url, options ).catch( e => {
             console.log( `[OOHTMLSSR]: Error loading subresource ${ url }` );
